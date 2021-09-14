8dcb30d9bf9fd71183a08a463fb341336ae0d75d
Agaviria <gaviria.alberto@gmail.com>
2021-09-14 09:28:54 -0400

    14-sep

--- Python/Ejercicios/invertir_cadena.py
+++ Python/Ejercicios/invertir_cadena.py
@@ -2,12 +2,13 @@ def invertir(algo):
     nuevo=[]
     inicio=len(algo)
     for i in range(inicio):
-        nuevo.append(algo.index(inicio)+1)
+        nuevo.append(algo[inicio-1])
         inicio=inicio-1
     return nuevo
 
 def invertir_ca(algo):
     nuevo_ca=""
+    global palindrome
     for i in algo:
         nuevo_ca= i + nuevo_ca
     if algo==nuevo_ca:
@@ -16,10 +17,10 @@ def invertir_ca(algo):
         palindrome=False
     return nuevo_ca
 
-
-mylista=[1,2,3,4,5,6,7,8,9]
-mycadena="radar"
+mylista=["a","b",1,2,3,4,5,6,7,8,9]
+mycadena="solos"
 print(mycadena)
 print(invertir_ca(mycadena))
 print(palindrome)
-
+print(mylista)
+print(invertir(mylista))
